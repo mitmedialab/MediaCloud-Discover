4d02a39937f89cb0fc8d5943b3fcb9a9ec12e11c
Sands Fish <sandsfish@gmail.com>
2018-03-20 12:15:09 -0400

    Documentation and tags_id addition.

--- static/js/MCContext.js
+++ static/js/MCContext.js
@@ -1,9 +1,22 @@
 function MCContext(data) {
 
+	// The name of the selected entity we are exploring
 	this.currentEntity;
-	this.tag_sets_id;
 
+	// Not sure we need these if we're grabbing the whole userData object (below)
+	this.tag_sets_id;
+	this.tags_id;
+	
+	// For convenience, just loading the whole set of properties from the entity here
+	this.userData;
+
+	// Defaulting Picker Entity Exploration to U.S. Collection
+	this.country_id = 9139487;
+
+	// For easy access to the camera
 	this.camera;
+
+	// Keeping track of what scene we are currently in
 	this.currentScene = data.scene;
 
 	// TODO: Fill out links to Dashboard or Other
