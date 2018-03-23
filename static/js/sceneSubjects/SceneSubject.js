476de96cc6ad38e472e4d6cc33c928174359ff9e
Sands Fish <sandsfish@gmail.com>
2018-03-23 10:00:20 -0400

    Documentation for unconventional pinning of objects to camera.

--- static/js/sceneSubjects/SceneSubject.js
+++ static/js/sceneSubjects/SceneSubject.js
@@ -3,9 +3,12 @@ function SceneSubject(scene) {
 	const radius = 2;
 	const mesh = new THREE.Mesh(new THREE.IcosahedronBufferGeometry(radius, 2), new THREE.MeshStandardMaterial({ flatShading: true }));
 
-	MC_CONTEXT.camera.add(mesh);
+	// Pinning Objects To The Camera 
+	// 	(requires camera to be unconventionally added to the scene)
+	//	(requires position to be set after being added to the camera)
+	// https://stackoverflow.com/questions/17218054/how-to-put-an-object-in-front-of-camera-in-three-js
+	// MC_CONTEXT.camera.add(mesh);
 	mesh.position.set(-30, 15, -60);
-	// scene.add(mesh);
 
 	this.update = function(time) {
 		const scale = Math.sin(time)+2;
