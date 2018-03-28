d899e7eb3d7950255f8d370527871ffa4877d6d3
Sands Fish <sandsfish@gmail.com>
2018-03-28 18:05:42 -0400

    First attempt at a util class implementation. Not finished or integrated.

--- static/js/libs/Toolbox.js
+++ static/js/libs/Toolbox.js
@@ -1,10 +1,28 @@
-function Toolbox() {
+function Toolbox() {}
 
-	this.update = function(time) {
+    let txtMesh = null;
+    const textMaterial = new THREE.MeshPhongMaterial( { color: 0xffffff } );
+    
+/////////////////////////////////////////////////////////////////////////
+Toolbox.addText = function( t, pos, group ) {
 
-	}
+    var textGeo = new THREE.TextGeometry( t, {
+        font: font,
+        size: 2,
+        height: 0,
+        curveSegments: 12,
+    });
+    textGeo.computeBoundingBox();
+    // textGeo.computeVertexNormals();
+    // textGeo.center();
+    
+    txtMesh = new THREE.Mesh( textGeo, textMaterial );
+    txtMesh.position.x = pos.x + 10;
+    txtMesh.position.y = pos.y;
+    txtMesh.position.z = pos.z;
+    txtMesh.visible = false;
+    // mesh.castShadow = true;
+    // mesh.receiveShadow = true;
 
-	this.tool = function(param) {
-		
-	}
+    group.add( txtMesh );
 }
