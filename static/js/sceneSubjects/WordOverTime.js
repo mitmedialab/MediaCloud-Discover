e5bad32d36ed22b5aa19f9b868213ba2a04ec587
Sands Fish <sandsfish@gmail.com>
2018-04-01 19:30:21 -0400

    Adding line chart code to WordOverTime

--- static/js/sceneSubjects/WordOverTime.js
+++ static/js/sceneSubjects/WordOverTime.js
@@ -5,11 +5,73 @@
 
 function WordOverTime(scene) {
 
+    this.loadWordCharts = function( word ) {
+
+        $.getJSON( `/word_over_time/${word}`, function( freq_data ) {
+            
+            var values = $.map(freq_data, function(value, key) { return value });
+            values = values.slice(0, values.length-3);
+            const mesh = createLineChart( values, -100, 0xffa700 );
+            
+            scene.add(mesh)
+        });
+    }
+    this.loadWordCharts( 'obama' );
+
+
+    /////////////////////////////////////////////////////////////////////
+    function createLineChart(frequency_data, z_offset, color) {
+
+        let points = [];
+        let x = 0, y = 0, x_offset = 20;
+        const extrudeSettings2 = { amount: 4, bevelEnabled: false };
+
+        // Map count values down to 0-100
+        let out_min = 0;
+        let out_max = 100;
+        let in_min = Math.min.apply(null, frequency_data);
+        let in_max = Math.max.apply(null, frequency_data);
+        frequency_data = frequency_data.map(x => ((x - in_min) / (in_max - in_min)) * (out_max - out_min) + out_min);
+
+        // Pad Data with Zeros
+        const values = [0, 0, 0].concat(frequency_data, [0, 0, 0]);
+
+        // Define points along Z axis
+        for( var i = 0; i < values.length; i++ ) {
+            x = x + x_offset;
+            var v = new THREE.Vector3(x, values[i], 1);
+            console.log(v);
+            points.push(v);
+        }
+
+        // TODO: Explore curve types
+        var curve = new THREE.CatmullRomCurve3( points );
+
+        const spline_points = curve.getPoints( 400 );
+        const shape = new THREE.Shape( spline_points );
+
+        var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings2 );
+        var material = new THREE.MeshPhongMaterial( {
+                            color: color,
+                            emissive: color,
+                            side: THREE.DoubleSide,
+                            flatShading: true
+                        } );
+
+        var mesh = new THREE.Mesh( geometry, material );
+        mesh.position.y = -100;
+        mesh.position.x = -400;
+        mesh.position.z = z_offset;
+        mesh.receiveShadow = true;
+
+        return mesh;
+    }
 
     /////////////////////////////////////////////////////////////////////////
     this.update = function(time) {
 
-    	
+    	// no-op
+        // TODO: Any animation, unless this is triggered by events
     }
 
 
@@ -20,4 +82,5 @@ function WordOverTime(scene) {
     	scene.add(subscene);
     }
     this.init();
+
 }
