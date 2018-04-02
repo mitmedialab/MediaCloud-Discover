078f8965c815bb33601758387696db55508070b0
Sands Fish <sandsfish@gmail.com>
2018-04-02 16:33:19 -0400

    Load sentences based on current MC_CONTEXT; Copy all metadata fields from entity to THREE.js userData object; clean up old notes.

--- static/js/sceneSubjects/Picker.js
+++ static/js/sceneSubjects/Picker.js
@@ -3,12 +3,6 @@
 // Picker Scene
 /////////////////////////////////////////////////////////////////////////
 
-// TODO: 
-//
-//  Add Country Outlines: https://github.com/djaiss/mapsicon
-//  Integrated Text in WebGL: https://webglfundamentals.org/webgl/lessons/webgl-text-html.html
-
-
 function Picker(scene) {
 
     var subscene = new THREE.Object3D();
@@ -37,18 +31,15 @@ function Picker(scene) {
 
         } else {
 
-            if(entity.userData.type == 'word') {
+            if(MC_CONTEXT.userData.type == 'word') {
                 sentenceScene.loadSentences( MC_CONTEXT.country_id, MC_CONTEXT.userData.label );
             } else {
                 sentenceScene.loadSentences( MC_CONTEXT.country_id, MC_CONTEXT.userData.tags_id );
             }
 
         }
-
-        // if entity is a term, call with the text of the term
-        // else, if the entity is an entity_id, call with the entity_id 
-        // as the second 
     });
+
 
     // Load Entities for New Selected Country //
     $("#countries").change(function(e) {
@@ -133,54 +124,23 @@ function Picker(scene) {
 
 
     /////////////////////////////////////////////////////////////////////////
-	// Instantiate Entity Objects and Add to Entities Array
-	//
 	this.loadEntities = function( country_id ) {
 
-        console.log(`Loading Entities from ${country_id}.`)
+        // Instantiate Entity Objects and Add to Entities Array
 
         var built_entities = new THREE.Group();
         built_entities.name = 'entities';
-
         var all_entities = subscene.children[0];
-
-        // If scene already has entities loaded 
-        //  (i.e. after the initial load) 
-        //  fade all and remove
-        
-/*
-        if(all_entities !== undefined) {
-            
-            for (var i = 0; i < all_entities.children.length; i++) {
-                
-                // Each entity is a group containing geometry and a label
-                // This will fade the geometry.
-                // TODO: Either visible: false or fade labels as well
-                let e = all_entities.children[i].children[0];
-
-                var t = new TWEEN.Tween( e.material ).to( { opacity: 0 }, 1000 )
-                            .easing( TWEEN.Easing.Linear.None)
-                            .onComplete(function(){ 
-                                all_entities.remove(all_entities.children[i]);
-                            });
-                t.start();
-
-
-                // console.log(all_entities.children[i]);
-                // all_entities.remove(all_entities.children[i]);
-            }
-        }
-*/
 
         $.getJSON( `/country_entities/${country_id}`, function( country_data ) {
 
-            // Create Entities For Each Collection Data Element //
+            // Create Entities For Each Collection Data Element
     	    for(var i = 0; i < country_data.length; i++) {
 
-                // This Group Holds The Entity Geometry and Label //
+                // This Group Holds The Entity Geometry and Label
                 var entityGroup = new THREE.Group();
 
-                // Create Entity Geometry //
+                // Create Entity Geometry
                 var geometry = new THREE.IcosahedronBufferGeometry(5, 0);
                 var material = new THREE.MeshPhongMaterial( {
                          color: getRandomColor(),
@@ -189,26 +149,12 @@ function Picker(scene) {
                          flatShading: true
                 } );
                 var entity = new THREE.Mesh(geometry, material);
-                
-                //////////////////////////////////////////////////
-                //           Data to Entity Mappings            //
-                //////////////////////////////////////////////////
 
-                // Add all entity metadata to Mesh userData
-                entity.userData.append( country_data[i] );
-
-                entity.userData.name = country_data[i].label;
+                // Add entity label as Object3D name for convenience
                 entity.name = country_data[i].label;
-
-                // entity.userData.type = country_data[i].type;
-                // entity.userData.tags_id = country_data[i].tags_id;
-                // entity.userData.tag = country_data[i].tag;
-                // entity.userData.count = country_data[i].count;
-                // entity.userData.tag_sets_id = country_data[i].tag_sets_id;
-
-                // FIXME: Only for Media Sources? 
-                //        Or do we want to URL to Dashboard pages?
-                // entity.userData.url = data[i].url;
+                
+                // Add all entity metadata fields to mesh userData
+                entity.userData = $.extend( entity.userData, country_data[i] );
 
                 // Randomize Initial Location //
                 entity.position.x = THREE.Math.randFloatSpread( 200 );
@@ -287,26 +233,6 @@ function Picker(scene) {
     /////////////////////////////////////////////////////////////////////////
     function drawChart() {
 
-        // 1. Specify values
-        // 2. Specify x-interval
-        // 3. Specify zero origin on screen
-        // 4. Iterate through values
-        //      4a. Add vertex based on x-origin, x-interval, and y-origin
-        // 
-
-        // var origin = new THREE.Vector3( -100, 0, 0 )
-
-        // var material = new THREE.LineBasicMaterial( {
-        //     color: 0xffffff,
-        //     linewidth: 1,
-        //     linecap: 'round', //ignored by WebGLRenderer
-        //     linejoin:  'round' //ignored by WebGLRenderer
-        // } );
-
-        // var shape = new THREE.Shape();
-        // heartShape.moveTo(...);
-
-
         var ctx = document.getElementById("chart");
         ctx.height = 125;
 
@@ -357,42 +283,5 @@ function Picker(scene) {
         });
     }
 
-
-    // TODO: Holy Shit this is inefficient. Solution here:
-    //  https://stackoverflow.com/questions/42829635/how-to-load-a-font-only-once-ts-three-js?rq=1
-
-
-
     this.init();
 }
-
-
-    /**
-
-    OK, what do we need here. 
-
-    0. A data pull from Media Cloud that loads (from a cache possibly) the various data we want to use as starting points
-    1. An array / particle system to track all of the Media Sources & Entities 
-    2. Perlin noise appropriate to having them float a bit randomly (if necessary) maybe just orbit like the stars
-    3. An update method that does the appropriate animation/rotation
-    4. Interaction handling methods for mouse-overs, click events
-        mesh.raycast ( raycaster, intersects );
-    5. Maybe an object external to this Scene that holds Entity data and interactions, and the ContextMenu
-    6. Any additional menu and interaction elements 
-
-    **/
-
-    // this.loadEntities = function() {
-        // 1. Get Data
-        // 2. Iterate
-        //      a. Create an Entity object
-        //      b. Init with data
-        //      c. Randomly locate in 3D space
-        //      d. Set visible properties to indicate type, etc.
-        //      e. Add to scene
-    // }
-    
-    // this.update = function(time) {
-        // 1. Iterate through Entities array
-        // 2. Update locations based on speed or simple rotation or location calculated in Entities 
-    // }
